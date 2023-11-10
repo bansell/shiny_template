662d5999f7d1a129724a92961a5cf286fb0ac9ec
bansell <bansell@github.com>
2023-11-10 11:55:59 +1100

    setup

---
+++ setup/pck_install_background_job.R
@@ -0,0 +1,18 @@
+# required ----------------------------------------------------------------
+
+install.packages('bslib')
+install.packages('tidyverse')
+install.packages('shiny')
+install.packages('shinyauthr')
+install.packages('sodium')
+install.packages('devtools')
+
+
+# optional ------------------------------------------------------------------
+
+devtools::install_github('bansell/tidyExt')
+
+install.packages('janitor')
+install.packages('here')
+install.packages('glue')
+
